ff393dade080531537b51b37e899010685553f6f
simoncyk <simoncyk@163.com>
2018-04-24 17:00:29 +0800

    generatorConfig  bulidingsucceed updata

---
+++ src/main/java/com/CrmSystem/controller/customer.java
@@ -0,0 +1,17 @@
+package com.CrmSystem.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: Administrator
+ * \* Date: 2018/04/24
+ * \* Time: 16:36
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@Controller
+public class customer {
+
+}
